054c8dd740b5b9be29e11864a22af5ea2540d452
dinmaduraimani <45810749+dinmaduraimani@users.noreply.github.com>
2019-01-12 10:28:51 +0530

    API  server initial setup

---
+++ app.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+
+const SERVER_PORT = 8080;
+
+let server = null;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'static/index.html'));
+});
+
+app.use( (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true;
+}));
+
+server = app.listen(SERVER_PORT, '0.0.0.0', () => {
+    const host = server.address().address;
+    const port = server.address().port;
+  
+    console.log('Example app listening at http://%s:%s', host, port);
+});
+  
